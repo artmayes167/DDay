91359ff001860bb74ae17e6678d3d6fd6cc7cab0
Mayes, Arthur E <arthur.e.mayes@accenture.com>
2015-11-10 13:46:56 -0800

    Added constants

---
+++ DDay/Constant/NDConstant.h
@@ -0,0 +1,56 @@
+//
+//  NDConstant.h
+//  NidecDemo
+//
+//  Created by Inturu, Ramanjaneyulu on 16/10/15.
+//  Copyright © 2015 Inturu, Ramanjaneyulu. All rights reserved.
+//
+
+#ifndef NDConstant_h
+#define NDConstant_h
+
+#define DEVELOPMENT TRUE
+#define USE_FIXTURES TRUE
+
+
+
+#define APP_FONT_BOLD           @"GillSans-Bold"
+#define APP_FONT                @"GillSans"
+#define APP_FONT_LIGHT          @"GillSans-Light"
+#define APP_FONT_Avenire        @"AvenirNext-Regular"
+#define APP_FONT_Avenire_BOLD   @"AvenirNext-Bold"
+#define APP_FONT_Avenire_DEMI_BOLD   @"AvenirNext-DemiBold"
+#define SERVER_DATE_FORMAT      @"yyyy-MM-dd'T'HH:mm:ssZ"
+#define DATE_FORMAT_TEXT        @"MM/dd/yyyy"
+#define IS_FIRST_LUNCH          @"IS_FIRST_LUNCH"
+
+//API Set_Configutation_Dynamically
+#define USE_APIGEE_PROD    0
+#define USE_APIGEE_TEST    1
+#define USE_FIXTURES_      2
+
+
+//Login Response
+#define AUTHENTICATION_TOKEN    @"AUTHENTICATION_TOKEN"
+#define REFRESH_TOKEN           @"REFRESH_TOKEN"
+#define TOKEN_TIME_OUT          @"TOKEN_TIME_OUT"
+#define USERNAME                @"USERNAME"
+#define SERVERURL               @"SERVERURL"
+#define REMEMBERME              @"REMEMBERME"
+#define PASSWORD                @"PASSWORD"
+#define DISPLAY_NAME            @"DISPLAY_NAME"
+#define USER_TITLE              @"USER_TITLE"
+#define USER_PICURL             @"USER_PICURL"
+#define RMS_USERID              @"RMS_USERID"
+#define COLLABORATOR_ID         @"COLLABORATOR_ID"
+#define CLIENT_ID_KEY           @"clientId"
+#define CLIENT_ID_VALUE         @""
+#define CLIENT_SECRET_KEY       @"clientSecret"
+#define CLIENT_SECRET_VALUE     @"600"
+
+//UIAlertView
+#define OK      @"OK"
+
+#define SHOW_LOGIN_SCREEN @"SHOW-LOGIN-SCREEN"
+
+#endif /* NDConstant_h */
